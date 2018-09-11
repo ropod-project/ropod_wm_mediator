f31d5082045dc253ead83ba7601e822f7fa88702
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2018-09-12 00:05:35 +0200

    Added action servers for making ROPOD specific queries
    - Changed all services to action servers
    - Added ropod specific abstraction to OSM query interface in wm_mediator

--- include/ropod_wm_mediator/wm_mediator.h
+++ include/ropod_wm_mediator/wm_mediator.h
@@ -1,73 +1,45 @@
 #ifndef WM_MEDIATOR_H
 #define WM_MEDIATOR_H
 
+// C++
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 #include <vector>
 #include <map>
+
+// ROS
 #include <ros/ros.h>
-#include <ropod_ros_msgs/ropod_sem_waypoint_list.h>
-#include <ropod_ros_msgs/ropod_demo_plan.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib/client/simple_action_client.h>
 
-// a waypoint is identified by a (semantic) name, and is a pose on the map
-typedef std::map<std::string, geometry_msgs::Pose> MapWaypoints;
-// an area is identified by a name, and consists of a list of waypoints
-typedef std::map<std::string, std::vector<std::string> > MapAreas;
-// a location is identified by a name, and consists of a list of areas
-typedef std::map<std::string, std::vector<MapAreas> > MapLocations;
+// ROPOD
+#include <ropod_ros_msgs/OSMNode.h>
+#include <ropod_ros_msgs/OSMWay.h>
+#include <ropod_ros_msgs/OSMRelation.h>
+#include <ropod_ros_msgs/OSMTag.h>
+#include <ropod_ros_msgs/OSMMember.h>
+#include <ropod_ros_msgs/Waypoint.h>
+#include <ropod_ros_msgs/Position.h>
+#include <ropod_ros_msgs/Shape.h>
 
-// a path is identified by a name, and consists of a list of locations
+#include <ropod_ros_msgs/OSMQueryAction.h> 
+#include <ropod_ros_msgs/GetWayptPositionAction.h> 
+#include <ropod_ros_msgs/GetWayptShapeAction.h> 
+
 
 class WMMediator
 {
 private:
     ros::NodeHandle nh_;
-    ros::Publisher plan_pub_;
-    ros::Subscriber goal_sub_;
-
-    std::string semantic_waypoints_file_;
-    std::string locations_file_;
-
-    MapWaypoints waypoints_;
-    MapLocations locations_;
-
-    /**
-     * callback for receiving list of semantic waypoints from com_mediator
-     *
-     * a ropod_demo_plan message is constructed and published (received by route navigation)
-     *
-     * Each corresponding semantic waypoint corresponds to a location.
-     * A location consists of a set of areas, which in turn consist of a set of waypoints
-     */
-    void goalCallback(const ropod_ros_msgs::ropod_sem_waypoint_list::ConstPtr &msg) const;
-
-    /**
-     * returns true if location exists in world model
-     */
-    bool locationExists(const std::string &location) const;
-
-    /**
-     * returns set of areas corresponding to a location
-     */
-    std::vector<MapAreas> getAreasForLocation(const std::string &location) const;
-
-    /**
-     * returns true if mapping from waypoint name to pose exists
-     */
-    bool waypointExists(const std::string &waypoint) const;
-
-    /**
-     * returns pose for a given waypoint
-     */
-    geometry_msgs::Pose getWaypointPose(const std::string &waypoint) const;
-
-    /**
-     * load mapping from names to poses from yaml file
-     */
-    void loadSemanticWaypoints();
-
-    /**
-     * load mapping from locations to areas to waypoints from yaml file
-     */
-    void loadLocations();
+    actionlib::SimpleActionServer<ropod_ros_msgs::GetWayptPositionAction> get_waypt_position_server;
+    actionlib::SimpleActionServer<ropod_ros_msgs::GetWayptShapeAction> get_waypt_shape_server;
+    actionlib::SimpleActionClient<ropod_ros_msgs::OSMQueryAction> ac;
+    ropod_ros_msgs::OSMQueryResult osm_query_result;
+    
+    void OSMQueryResultCb(const actionlib::SimpleClientGoalState& state,const ropod_ros_msgs::OSMQueryResultConstPtr& result);
+    void get_waypt_position_execute(const ropod_ros_msgs::GetWayptPositionGoalConstPtr& goal);
+    void get_waypt_shape_execute(const ropod_ros_msgs::GetWayptShapeGoalConstPtr& goal);
 
 public:
     WMMediator();
