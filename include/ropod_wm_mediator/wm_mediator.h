bb8f92d064e017b59b2811013c8c83cfa6136845
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2018-11-22 12:36:20 +0100

    Implemented basic structure using OBL wrapper

--- include/ropod_wm_mediator/wm_mediator.h
+++ include/ropod_wm_mediator/wm_mediator.h
@@ -14,18 +14,22 @@
 #include <actionlib/client/simple_action_client.h>
 
 // ROPOD
-#include <ropod_ros_msgs/OSMNode.h>
-#include <ropod_ros_msgs/OSMWay.h>
-#include <ropod_ros_msgs/OSMRelation.h>
-#include <ropod_ros_msgs/OSMTag.h>
-#include <ropod_ros_msgs/OSMMember.h>
 #include <ropod_ros_msgs/Waypoint.h>
 #include <ropod_ros_msgs/Position.h>
 #include <ropod_ros_msgs/Shape.h>
-
-#include <ropod_ros_msgs/OSMQueryAction.h> 
+ 
 #include <ropod_ros_msgs/GetWayptPositionAction.h> 
 #include <ropod_ros_msgs/GetWayptShapeAction.h> 
+
+// OBL
+#include <osm_bridge_ros_wrapper/Area.h>
+#include <osm_bridge_ros_wrapper/Corridor.h>
+#include <osm_bridge_ros_wrapper/Elevator.h>
+#include <osm_bridge_ros_wrapper/Stairs.h>
+#include <osm_bridge_ros_wrapper/Room.h>
+#include <osm_bridge_ros_wrapper/LocalArea.h>
+
+#include <osm_bridge_ros_wrapper/WMQueryAction.h>
 
 
 class WMMediator
@@ -34,10 +38,10 @@ private:
     ros::NodeHandle nh_;
     actionlib::SimpleActionServer<ropod_ros_msgs::GetWayptPositionAction> get_waypt_position_server;
     actionlib::SimpleActionServer<ropod_ros_msgs::GetWayptShapeAction> get_waypt_shape_server;
-    actionlib::SimpleActionClient<ropod_ros_msgs::OSMQueryAction> ac;
-    ropod_ros_msgs::OSMQueryResult osm_query_result;
+    actionlib::SimpleActionClient<osm_bridge_ros_wrapper::WMQueryAction> wm_query_ac;
+    osm_bridge_ros_wrapper::WMQueryResult wm_query_result;
     
-    void OSMQueryResultCb(const actionlib::SimpleClientGoalState& state,const ropod_ros_msgs::OSMQueryResultConstPtr& result);
+    void WMQueryResultCb(const actionlib::SimpleClientGoalState& state,const osm_bridge_ros_wrapper::WMQueryResultConstPtr& result);
     void get_waypt_position_execute(const ropod_ros_msgs::GetWayptPositionGoalConstPtr& goal);
     void get_waypt_shape_execute(const ropod_ros_msgs::GetWayptShapeGoalConstPtr& goal);
 
